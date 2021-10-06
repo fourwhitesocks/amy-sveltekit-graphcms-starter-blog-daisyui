40cb4849d5ca7f237c4f45db89b3ea0e27d32965
amy stout <asbilly92@gmail.com>
2021-10-06 08:30:24 -0400

    Made adjustments to svelte.config and added Netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess'
-import vercel from '@sveltejs/adapter-vercel'
+//import vercel from '@sveltejs/adapter-vercel'
+import adapter from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,7 +12,8 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
-    adapter: vercel(),
+    //adapter: vercel(),
+    adapter: adapter()
   },
 }
 
