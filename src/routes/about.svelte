6703082d532f4b6e02b9aa45eeff2bd4e1277dcd
amy stout <asbilly92@gmail.com>
2021-10-06 06:52:46 -0400

    Add sample files

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<div
+  class="hero min-h-[90vh]"
+  style="background-image: url(&quot;https://picsum.photos/id/1005/1600/1400&quot;);"
+>
+  <div class="hero-overlay bg-opacity-60" />
+  <div class="text-center hero-content text-neutral-content">
+    <div class="max-w-md">
+      <h1 class="mb-5 text-5xl font-bold">GraphCMS blog starter</h1>
+      <p class="mb-5">
+        GraphCMS blog starter built with SvelteKit, Tailwind and
+        daisyUI
+      </p>
+    </div>
+  </div>
+</div>
